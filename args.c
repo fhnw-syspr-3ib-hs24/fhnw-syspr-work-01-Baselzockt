786d7f95a25799f042f27bb70aec7258aafcaca0
Baselzockt <baselzockt@gmail.com>
2024-09-24 17:17:46 +0200

    copied over from wrong (old) repo

---
+++ args.c
@@ -0,0 +1,27 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "args.h"
+
+
+int main(int argc, char *argv[]) {
+    for (int i = 1; i < argc; i++) {
+        if (!is_alpha(argv[i])) {
+            printf("Only characters are allowed as arguments");
+            return -1;
+        }
+
+        printf("Argument nr %d: %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
+
+int is_alpha(char *s) {
+    while (*s != *"\0") {
+        if (!isalpha(*s)) {
+            return 0;
+        }
+        s++;
+    }
+    return 1;
+}
---
+++ size_of.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("size of char %lu\n", sizeof(char));
+    printf("size of int %lu\n", sizeof(int));
+    printf("size of long %lu\n", sizeof(long));
+    printf("size of float %lu\n", sizeof(float));
+
+    return 0;
+}
